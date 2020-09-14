4e596571ff8d93d22ff1e5b48fb0652161c0d791
rjmchi <robert@rjmprogramming.com>
2020-09-14 00:59:30 -0500

    admin dashboard

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -3,5 +3,11 @@
 @section('content')
 <div class="container">
     <h1>ChicagoPotters Admin Panel</h1>
+    <ul class="list-group">
+        <li class="list-group-item">Artists</li>
+        <li class="list-group-item">Add Piece</li>
+        <li class="list-group-item">Add Category</li>
+        <li class="list-group-item"><a href="{{route('album.index')}}">Albums</a></li>
+    </ul>     
 </div>
 @endsection
