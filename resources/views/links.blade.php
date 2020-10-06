28477ea2a3b7dc005d81aaa89a812ceffc38f367
rjmchi <robert@rjmprogramming.com>
2020-10-06 01:50:49 -0500

    Adds Links

--- resources/views/links.blade.php
+++ resources/views/links.blade.php
@@ -3,5 +3,31 @@
 @section('content')
 <div class="container">
    <h1>Links</h1>
+<p>Artists</p>
+		<ul class="cp-list">
+			<li><a href="http://www.lancefriedmansculpture.com" target="_blank">Lance Friedman &mdash; Sculpture &mdash; Chicago</a></li>
+			<li><a href="http://muralsbyroberta.com" target="_blank">Roberta Skolnik &mdash; Murals &amp; Painting &mdash; Gilbert, AZ</a></li>
+			<li><a href="http://npgraphx.com/" target="_blank">Nancy Pirri &mdash; Sculpture &mdash; Chicago</a></li>
+			<li><a href="http://emilymurphy.com/" target="_blank">Emily Murphy &mdash; Soda Fired Ceramics- Minneapolis, MN</a></li>
+			<li><a href="http://potteryblog.com" target="_blank">Pottery Blog : Emily Murhphy</a> </li>
+			<li><a href="http://www.casbah3d.com/" target="_blank">Catherine Schwalbe</a></li>
+			<li><a href="http://www.pinderpottery.com" target="_blank">Joe Pinder &mdash; Pottery &mdash;Galena, IL</a></li>
+			<li><a href="http://www.eshelmanpottery.com/" target="_blank">Paul Eshelman &mdash; Pottery &mdash; Elizabeth, IL</a></li>
+			<li><a href="http://www.dutchlakepottery.com" target="_blank">Jeff Rottman &mdash; Pottery &mdash; Grand Rapids, MI</a></li>
+			<li><a href="http://aurinkophoto.com" target="_blank">Susan Aurinko &mdash; Photography </a></li>
+			<li><a href="http://www.pattykochaver.com/" target="_blank">Patty Kochaver &mdash; Ceramics</a></li>
+			<li><a href="http://www.robertapolfusceramics.com/" target="_blank">Roberta Polfus &mdash; Ceramics &mdash; Oak Park, IL</a></li>
+			<li><a href="http://www.birkenheuerphotography.com/" target="_blank">Doug Birkenheuer &mdash; Photography &mdash; Chicago</a></li>
+			<li><a href="https://www.redgategallery.org/" target="_blank">Red Gate Gallery &mdash; Puerto Vallarta</a></li>
+		</ul>
+		<p>Organizations</p>
+		<ul class="cp-list">
+			<li><a href="http://nceca.net/" target="_blank">NCECA</a></li>
+			<li><a href="http://www.caconline.org/" target="_blank">Chicago Artists Coalition</a></li>
+			<li><a href="http://www.ceramics.org/potterscouncil/" target="_blank">Potters Council</a></li>
+			<li><a href="http://www.chicagoartistsresource.org/" target="_parent">Chicago Artist Resource</a></li>
+			<li><a href="http://www.lillstreet.com/" target="_blank">Lillstreet Art Center</a></li>
+			<li><a href="http://www.bethelhorizons.org/Horizons/Art_Ventures/Art_ventures.html" target="_blank">Art Ventures &mdash; Dodgeville, WI</a></li>
+		</ul>   
 </div>
 @endsection
